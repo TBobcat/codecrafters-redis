269c8a55f0365b41cf336cb34a0296869b3a1e40
Tom Wang <xiangyuantech@gmail.com>
2023-06-20 14:45:48 -0400

    ping response

--- app/main.py
+++ app/main.py
@@ -7,8 +7,10 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    conn, addr = server_socket.accept() # wait for client, this is synchronous ?  won't execute code belwo until client sends request
+    print("new connection accepted ! ")
+    
+    conn.send(b"+OK\r\n")
 
 if __name__ == "__main__":
     main()
