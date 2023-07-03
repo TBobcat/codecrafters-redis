41ae298c790540990b09c3476e26a43adde3c807
Tom Wang <xiangyuantech@gmail.com>
2023-07-03 13:47:48 -0400

    echo task commit 1

--- app/main.py
+++ app/main.py
@@ -16,14 +16,36 @@ async def main():
 
 # this handler needs the while loop to keep opening for requests
 async def handler(reader, writer):
-    while True:        
+    while True:    
+        print("new connection accepted ! ")    
         data = await reader.read(100)
         # checks data stream so server doesn't crash and wait for data finish sending
         if not data:
             break
-        print("new connection accepted ! ")
-        writer.write(b"+PONG\r\n")
 
+        lst = data.split()
+        # cheating PING command with this case
+        if data == b'*1\r\n$4\r\nping\r\n':
+            writer.write(b'+PONG\r\n')
+        else:
+            echo_idx = lst.index(b'echo')
+            return_lst = lst[(echo_idx+1):]
+            parsed_str = parse(return_lst)
+            writer.write(bytes(parsed_str, encoding='utf-8'))
+
+def parse(lst):
+    """"
+    parse input list of bytes to a byte string that echo returns, in redis protocol
+    """
+    length = 0
+    return_s = ""
+
+    for byte_s in lst:
+        return_s+=bytes.decode(byte_s)+'\r\n'
+        if bytes.decode(byte_s)[0] != "$":
+            length += 1
+    
+    return "*" + str(length) + '\r\n' + return_s
 
 if __name__ == "__main__":
     asyncio.run(main())
