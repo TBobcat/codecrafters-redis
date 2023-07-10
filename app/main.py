6814a3109cc6cb07b377e129ef964a452b78aa91
Tom_Rum_Code <xiangyuantech@gmail.com>
2023-07-09 22:20:27 -0400

    passed set and get stage (#4)
    * test async
    
    * test 2
    
    * conncurrency test 3
    
    * echo task commit 1
    
    * task echo command commit 2
    
    * passed echo task
    
    * delete pyc file
    
    * make set and get
    
    * fix code for echo

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import asyncio
 
 PORT = 6379
 HOST = "localhost"
+mem = {}
 
 async def main():
     print("Logs from your program will appear here!")
@@ -15,14 +16,56 @@ async def main():
         await server.serve_forever()
 
 # this handler needs the while loop to keep opening for requests
+# takes socket data in redis protocol, and set/get if needed, and 
+# writes response
 async def handler(reader, writer):
-    while True:        
+    while True:    
+        print("new connection accepted ! ")    
         data = await reader.read(100)
         # checks data stream so server doesn't crash and wait for data finish sending
         if not data:
             break
-        print("new connection accepted ! ")
-        writer.write(b"+PONG\r\n")
+
+        lst = parse(data)
+        # cheating PING command with this case
+        if data == b'*1\r\n$4\r\nping\r\n':
+            writer.write(b'+PONG\r\n')
+        elif 'echo' in lst:
+            print(lst)
+            writer.write(bytes('+' + lst[4] + '\r\n', encoding='utf-8'))
+        elif 'set' in lst:
+            mem[lst[4]] = lst[6]
+            print(mem)
+            writer.write(b'+OK\r\n')
+        elif 'get' in lst:
+            if lst[4] in mem:
+                value = mem[lst[4]]
+                writer.write(bytes('+' + value +'\r\n', encoding='utf-8'))
+            else:
+                writer.write(bytes("-1\r\n", "utf-8"))
+
+            
+
+# decode b'*3\r\n$3\r\nset\r\n$1\r\na\r\n$1\r\n3\r\n' into list of strings
+def parse(lst):
+    """"
+    parse input list of bytes to a byte string that echo returns, in redis protocol
+    """
+    decoded_slst = []
+    split_list = lst.split()
+    for s in split_list:
+        s = bytes.decode(s)
+        decoded_slst.append(s)
+    
+    print(decoded_slst)
+    return decoded_slst
+
+def parse_simple_string(lst):
+    """
+    return the single full string of echo command
+    """
+    s = bytes.decode(lst[1])
+    return "+" + s + "\r\n"
 
 
 if __name__ == "__main__":
